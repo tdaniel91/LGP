f59b04e6fccab57d3a8669df2fe10778e588b81d
t.daniel <t.daniel@campus.fct.unl.pt>
2013-11-29 15:21:05 +0000

    init
    proj vazio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LGP::Application.config.secret_key_base = '734da50f3650a50931cf0410a70e0a276aa58038d8a1ff310084d31821c107b3a0d28ceaf3f8546af5a5e0590d1637e2ed9d481e3121ac3f6d552a2577647944'
